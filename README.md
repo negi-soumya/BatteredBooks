172fd6d541c965cf4b0ef9876d8d6edd43b9c8bf
negi-soumya <soumya.negi97@gmail.com>
2017-10-30 14:20:53 +0530

    first look

---
+++ README.md
@@ -0,0 +1,5 @@
+# BatteredBooks
+Website for selling secondhand books  
+Repo made for IWP(Internet and Web Programming) project, Sem 3  
+**Used:** Bootstrap, jQuery(effects & AJAX),PHP  
+Code developed on **xampp**  
---
+++ js/main.js
@@ -0,0 +1,24 @@
+
+/************jQuery code**************/
+
+//to stop preloading.gif
+$(document).ready(function(){
+    
+        $(".loaded").fadeOut();    
+        $(".preloader").delay(800).fadeOut();
+    });
+
+
+// for smooth scrolling on clicking <a>
+$(document).ready(function(){ 
+  $("a").on('click', function(event) {   
+    if (this.hash !== "") {
+      event.preventDefault(); // to prevent default instant navigation
+      var hash = this.hash; //hash is the sectionid
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top}, 1200, function(){ 
+        						 window.location.hash = hash;});
+   							 } 
+ 			      });
+});
+
